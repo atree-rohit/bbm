4f5c18175b459bce394bd05f15c260b8003b4289
Rohit <rohit.george@atree.org>
2020-11-13 11:43:09 +0530

    added logging to ibp model

--- app/Models/IBP.php
+++ app/Models/IBP.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class IBP extends Model
 {
-    use HasFactory;
+    use LogsActivity, HasFactory;
+
+    protected $fillable = ["id", "createdBy", "placeName", "flagNotes", "noOfIdentifications", "createdOn", "associatedMedia", "locationLat", "locationLon", "locationScale", "fromDate", "toDate", "rank", "scientificName", "commonName", "family", "genus", "species", "state"];
+
+    protected static $logAttributes = ["id", "createdBy", "placeName", "flagNotes", "noOfIdentifications", "createdOn", "associatedMedia", "locationLat", "locationLon", "locationScale", "fromDate", "toDate", "rank", "scientificName", "commonName", "family", "genus", "species", "state"];
+
+    protected static $logFillable = true;
+    protected static $logOnlyDirty = true;
 }
