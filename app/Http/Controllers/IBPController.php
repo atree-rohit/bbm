0a12ab6dff5484c6c2d5e7e1f238754db77326f5
Rohit <rohit.george@atree.org>
2020-11-13 11:14:36 +0530

    added IBP mrc

---
+++ app/Http/Controllers/IBPController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IBP;
+use Illuminate\Http\Request;
+
+class IBPController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\IBP  $iBP
+     * @return \Illuminate\Http\Response
+     */
+    public function show(IBP $iBP)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\IBP  $iBP
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(IBP $iBP)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\IBP  $iBP
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, IBP $iBP)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\IBP  $iBP
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(IBP $iBP)
+    {
+        //
+    }
+}
