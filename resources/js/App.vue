376c80e2b3948f892bdf84d79cc98f0e5a74ad8d
Rohit George <rohit.george@atree.org>
2023-03-28 12:14:00 +0530

    setup orject for vue

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <species-page />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SpeciesPage from './components/SpeciesPage.vue'
+export default defineComponent({
+    name: "App",
+    components: {
+        SpeciesPage
+    }
+})
+</script>
