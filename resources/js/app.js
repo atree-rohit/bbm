fee1e5905333817e3c7b678b70dd912195de478f
Rohit George <rohit.george@atree.org>
2023-03-28 12:14:33 +0530

    vue setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+// import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
+import App from'./App.vue'
+
+const app = createApp(App)
+
+
+app.mount('#app')
