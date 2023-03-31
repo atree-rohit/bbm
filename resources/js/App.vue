c33b4e9ffd7e0ddb8f8a4a2308a11e53ac26d9bb
Rohit George <rohit.george@atree.org>
2023-03-31 12:03:34 +0530

    cleanup and added new components

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,38 +1,40 @@
-
+<style scoped>
+</style>
+ 
 <template>
-    <!-- <species-page /> -->
-    Stats
-    <pre>
-        {{ sizes }}
-    </pre>
+    <div class="main-wrapper">
+		<navbar />
+		<results />
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { mapState } from 'vuex'
 import store from './store/index.js'
+import Navbar from './components/Navbar.vue'
+import Results from './components/Results.vue'
 import SpeciesPage from './components/SpeciesPage.vue'
 export default defineComponent({
     name: "App",
     components: {
-        SpeciesPage
+      Navbar,
+      Results,
+      SpeciesPage,
+    },
+    data(){
+        return {
+            
+        }
     },
     created(){
         store.dispatch('getAllData')
     },
     computed:{
         ...mapState([
-            'taxa',
-            'users',
-            'observations'
         ]),
-        sizes(){
-            return {
-                taxa: JSON.stringify(this.taxa).length,
-                users: JSON.stringify(this.users).length,
-                observations: JSON.stringify(this.observations).length
-            }
-        }
+    },
+    methods:{
     }
 
 })
