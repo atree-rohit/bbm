df0dd96d21733f67d20cd20f978df5158cb79b96
Rohit George <rohit.george@atree.org>
2023-04-03 11:17:38 +0530

    updated to match species tab

--- resources/js/components/SelectUser.vue
+++ resources/js/components/SelectUser.vue
@@ -1,23 +1,34 @@
 <style scoped>
-    .users-wrapper{
+    .users-select-wrapper{
         height: 100%;
         display: grid;
-        grid-template-columns: 1fr 3fr;
-        gap: 1rem;
+        grid-template-rows: 1fr 2fr;
+        grid-template-columns: auto;
+        justify-content: center;
     }
-    .users-wrapper .filters{
-        border: 1px solid var(--clr-bg-grey);
-        border-radius: var(--border-radius);
-        padding: .5rem;
+    .user-filters{
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        align-items: center;
+        padding: 0rem;
+        grid-gap: 0rem;
+        min-width: 90vw;
+    }
+
+    .portals{
         display: flex;
-        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: center;
+        padding: 0rem;
     }
-
-    .filters .portals, .filters .search{
-        flex: 1 1 0;
-
+    .search input{
+        width: 100%;
+    padding: 0.25rem;
+    border: 1px solid var(--clr-bg-green);
+    border-radius: 0.25rem;
     }
-    .users-wrapper .users{
+    .users{
         border: 1px solid var(--clr-bg-grey);
         border-radius: var(--border-radius);
         overflow-y: auto;
@@ -26,6 +37,7 @@
         justify-content: center;
         flex-wrap: wrap;
         gap: 0.25rem 0.5rem;
+        width: 100%;
     }
 
     .users .chip{
@@ -68,20 +80,63 @@
         color: var(--clr-text-green);
     }
 
+    .btn{
+        border: 1px solid var(--clr-bg-blue);
+        color: var(--clr-bg-blue);
+        padding: .5rem 0.25rem;
+        font-size: 0.75rem;
+        border-radius: .5rem;
+    }
+
+
+    .btn.selected{
+        border: 1px solid transparent;
+        color: var(--clr-text-white);
+        background: var(--clr-bg-green);
+        
+    }
+
+    .btn:hover{
+        cursor: pointer;
+        background: var(--clr-bg-light-blue);
+    }
+
+    @media screen and (min-width: 800px) {
+        .users-select-wrapper{
+            border-radius: var(--border-radius);
+            padding: .5rem;
+            grid-template-columns: 1fr 4fr;
+            grid-template-rows: auto;
+            justify-content: space-between;
+        }
+        .user-filters{
+            min-width: 19vw;
+        }
+    }
+
 </style>
 
 <template>
-    <div class="users-wrapper">
-        <div class="filters">
-            <div class="portals">Portals</div>
+    <div class="users-select-wrapper">
+        <div class="user-filters">
+            <div class="portals">
+                <button
+                    class="btn"
+                    v-for="portal in portals"
+                    :key="portal.id"
+                    :class="portalClass(portal.id)"
+                    @click="selectPortal(portal)"
+                    v-text="portal.id"
+                />
+            </div>
             <div class="search">
-                <input type="text" v-model="search_string">
+                <input type="text" v-model="search_string" placeholder="Enter User Name to filter the list...">
             </div>
         </div>
         <div class="users">
             <div
                 class="chip"
-                v-for="user in users"
+                v-for="user in filtered_users"
                 :key="user.id"
                 :class="{ selected: selected.users.indexOf(user.id) > -1 }"
                 @click="selectUser(user)"
@@ -121,16 +176,37 @@ export default defineComponent({
                     name: "iFoundButterflies"
                 }
             ],
-            selected_portals: ["count", "inat", "ibp", "ifb"]
+            selected_portals: [],
+            search_string: ""
         }
+    },
+    created(){
+        this.selected_portals = this.portals.map((p) => p.id)
     },
     computed:{
         ...mapState([
             "users",
             "selected"
-        ])
+        ]),
+        filtered_users(){
+            return this.users.filter((user) => {
+                const portal_index = this.selected_portals.indexOf(user.source)
+                const search_index = user.name.toLowerCase().indexOf(this.search_string.toLowerCase())
+                return (portal_index > -1 && search_index > -1)
+            })
+        }
     },
     methods:{
+        portalClass(portal: string){
+            return (this.selected_portals.indexOf(portal) > -1) ? 'selected' : ''
+        },
+        selectPortal(portal: any){
+            if(this.selected_portals.indexOf(portal.id) > -1){
+                this.selected_portals = this.selected_portals.filter((p) => p != portal.id)
+            }else{
+                this.selected_portals.push(portal.id)
+            }
+        },
         selectUser(user: any){
             store.dispatch("selectUser", user.id)
         }
