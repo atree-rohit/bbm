1eac0a3d06eca7efaa4fd61f591cbd26ac621a46
Rohit George <rohit.george@atree.org>
2023-04-03 12:38:51 +0530

    new components

---
+++ resources/js/components/SelectLocation.vue
@@ -0,0 +1,17 @@
+<style scoped>
+</style>
+
+<template>
+    LOCATION
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+import { mapState } from 'vuex'
+import store from '../store'
+export default defineComponent({
+    name: "SelectLocation",
+
+})
+</script>
